887513737f2787c27f6879c3e667ee76acfce302
Daulet2006 <dosbauyr@gmail.com>
2025-11-29 14:31:55 +0500

    User class

---
+++ src/main/java/gitpractice/git/entity/User.java
@@ -0,0 +1,19 @@
+package gitpractice.git.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
